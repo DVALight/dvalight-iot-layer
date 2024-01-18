93fae7dd3f0fa079aed928e252093330fea38ab5
mykola2312 <nikolay_tihonov@i.ua>
2024-01-18 09:01:01 +0200

    implement DVA request parsing

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
 const DVA_DPORT = 1337
+
+const DVA_MAGIC uint64 = 0x544847494C415644
+const DVA_REQUEST_LEN = 8 + 4
+
+type DVARequest struct {
+	magic    uint64
+	deviceId uint32
+}
+
+func (req *DVARequest) parseDVARequest(buf []byte, len int) bool {
+	if len < DVA_REQUEST_LEN {
+		return false
+	}
+
+	req.magic = binary.LittleEndian.Uint64(buf[0:8])
+	req.deviceId = binary.LittleEndian.Uint32(buf[8:12])
+
+	return req.magic == DVA_MAGIC
+}
 
 func main() {
 	anyAddr, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("0.0.0.0:%d", DVA_DPORT))
@@ -16,9 +36,23 @@ func main() {
 		os.Exit(1)
 	}
 
+	devices := make(map[uint32]*net.UDPAddr)
+	var buf [512]byte
+
 	for {
-		var buf [512]byte
-		n, addr, err := conn.ReadFromUDP(buf[0:])
-		fmt.Printf("%d %s %s\n", n, addr, err)
+		len, addr, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		var req DVARequest
+		if req.parseDVARequest(buf[0:len], len) {
+			devices[req.deviceId] = addr
+
+			fmt.Printf("device %d request\n", req.deviceId)
+		} else {
+			fmt.Println("failed to parse DVA request")
+		}
 	}
 }
