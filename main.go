630881edfeee0e54dab9c4b3c82df621c6a832ab
mykola2312 <nikolay_tihonov@i.ua>
2024-01-18 13:24:21 +0200

    implement http request to backend

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
 )
 
@@ -11,6 +14,8 @@ const DVA_DPORT = 1337
 
 const DVA_MAGIC uint64 = 0x544847494C415644
 const DVA_REQUEST_LEN = 8 + 4
+
+const DVA_ENDPOINT = "http://localhost:4444/device"
 
 type DVARequest struct {
 	Magic    uint64
@@ -26,6 +31,12 @@ func (req *DVARequest) ParseDVARequest(buf []byte, len int) bool {
 	req.DeviceID = binary.LittleEndian.Uint32(buf[8:12])
 
 	return req.Magic == DVA_MAGIC
+}
+
+type DVAResponse struct {
+	Magic    uint64
+	DeviceID uint32 `json:"id"`
+	State    bool   `json:"state"`
 }
 
 func main() {
@@ -51,6 +62,28 @@ func main() {
 			devices[req.DeviceID] = addr
 
 			fmt.Printf("device %d request\n", req.DeviceID)
+
+			httpRes, err := http.Get(fmt.Sprintf("%s/%d", DVA_ENDPOINT, req.DeviceID))
+			if err != nil {
+				fmt.Printf("Error %s when requesting back-end", err)
+				continue
+			}
+
+			httpData, err := io.ReadAll(httpRes.Body)
+			httpRes.Body.Close()
+
+			if err != nil {
+				fmt.Printf("http body error: %s\n", err)
+				continue
+			}
+
+			var res DVAResponse
+			err = json.Unmarshal(httpData, &res)
+			if err != nil {
+				fmt.Printf("json error: %s\n", err)
+			}
+
+			fmt.Println(res)
 		} else {
 			fmt.Println("failed to parse DVA request")
 		}
