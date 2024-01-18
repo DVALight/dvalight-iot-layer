b96aaf57c91d16d45ac566d40871e76e4f3a944b
mykola2312 <nikolay_tihonov@i.ua>
2024-01-18 08:38:20 +0200

    init udp listener

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 const DVA_DPORT = 1337
 
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", "localhost:1337")
-	fmt.Printf("%s %s\n", addr, err)
+	anyAddr, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("0.0.0.0:%d", DVA_DPORT))
+	conn, err := net.ListenUDP("udp", anyAddr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for {
+		var buf [512]byte
+		n, addr, err := conn.ReadFromUDP(buf[0:])
+		fmt.Printf("%d %s %s\n", n, addr, err)
+	}
 }
